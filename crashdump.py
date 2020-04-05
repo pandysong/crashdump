ae6c4fafdc4255259d89958dd543a587777bb0d1
Pandy Song <pandy@bryture.com>
2020-04-05 10:25:25 +0800

    Refactor the code to support call trace analyzing
    The call trace printing in Android `logcat` has different format and
    each line indicates the elf file itself (to support multiple elf). This
    refactor improve the code to easily support multiple front end, in the
    future, we could add different front end by adding `address_from_xxx`
    functions.

--- crashdump.py
+++ crashdump.py
@@ -1,29 +1,78 @@
 import sys
 import subprocess
+import os
 
 if len(sys.argv) < 3:
-    print("usage: {} logfile elf_file".format(sys.argv[0]))
+    print("usage: {} logfile elf_file_or_path".format(sys.argv[0]))
     exit(1)
 
-for l in open(sys.argv[1]):
-    begin = l.find("[<")
-    if begin < 0:
-        continue
-    end = l.find(">]")
-    if end < 0:
-        continue
-    if begin >= end:
-        continue
-    addr = l[begin + 2: end]
-    commands = ["addr2line", "-a", addr, "-e",  sys.argv[2]]
-    # print(commands)
+
+def address_from_crash(lines, elf):
+    """yield tuple of address and original line
+    """
+    for l in lines:
+        begin = l.find("[<")
+        if begin < 0:
+            continue
+        end = l.find(">]")
+        if end < 0:
+            continue
+        if begin >= end:
+            continue
+        yield l[begin + 2: end], l[begin:], elf
+
+
+def address_from_trace(lines, path):
+    """yield tuple of address and original line
+    """
+    for l in lines:
+        begin = l.find(" pc ")
+        if begin < 0:
+            continue
+        end = l.find(" ", begin+4)
+        if end < 0:
+            continue
+        if begin >= end:
+            continue
+        elf_begin = l.find("/", end)
+        if elf_begin < 0:
+            continue
+        elf_end = l.find(" ", elf_begin)
+        if elf_end < 0:
+            continue
+        elf = os.path.join(path, l[elf_begin+1: elf_end])
+        yield l[begin + 4: end], l[begin:], elf
+
+
+def print_crash_dump(addr, l, elf):
+    commands = ["addr2line", "-a", addr, "-e",  elf]
+    #print(' '.join(commands))
     res = subprocess.Popen(commands,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT)
     stdout, stderr = res.communicate()
     if stderr:
         print(stderr.decode())
-        break
-    print("{}: {}".format(
-        stdout.decode().splitlines()[1],
-        l[begin:].strip()))
+        return
+    so = stdout.decode().splitlines()
+    # print(so)
+    if len(so) != 2:
+        print(so)
+        return
+    print("{}: {}".format(so[1], l), end="")
+
+
+lines = open(sys.argv[1]).readlines()
+e = sys.argv[2]
+
+c = (sum(1 for _ in address_from_crash(lines, e)), address_from_crash)
+t = (sum(1 for _ in address_from_trace(lines, e)), address_from_trace)
+
+if not c[0] and not t[0]:
+    print("failed to parse addresses")
+    exit(0)
+
+s = max(c, t)
+
+for addr, l, elf in s[1](lines, sys.argv[2]):
+    print_crash_dump(addr, l, elf)
